b6d39396f1ad24cd05aaef60758950f7ac1cbb3b
lantos1618 <l.leong1618@gmail.com>
2023-08-10 14:04:04 +0100

    add requirements, test twitter

--- src/test_similarities.py
+++ src/test_similarities.py
@@ -13,6 +13,30 @@ import numpy as np
 import pandas as pd
 
 
+import os
+import tweepy
+
+# Read the credentials from environment variables
+API_KEY = os.environ.get("TWITTER_API_KEY")
+API_SECRET_KEY = os.environ.get("TWITTER_API_SECRET_KEY")
+ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+
+
+
+# Set up the tweepy authorization
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+
+
+# Create the tweepy API object
+api = tweepy.API(auth)
+
+# Fetch user information by username
+user = api.get_user(screen_name="USERNAME")
+
+
 # Define the data structures
 class Tweet:
     def __init__(self, id: int, authorId: int, content: int):
